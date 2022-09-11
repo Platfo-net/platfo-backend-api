14b3efc48c667ae625886fe312f2461428fd98e3
Mohammad Serpoush <m.serpoush@sfg.ir>
2022-09-11 12:08:22 +0430

    merge heads

---
+++ alembic/versions/02a99b978664_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 02a99b978664
+Revises: be189d13b8ac, aff60aa8e45d
+Create Date: 2022-09-11 12:08:09.147691
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '02a99b978664'
+down_revision = ('be189d13b8ac', 'aff60aa8e45d')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
