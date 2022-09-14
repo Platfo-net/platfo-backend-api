12755b56216c7b940434608c2e0aa3f7919fb89d
Farhood Etaati <farhoodetaati@gmail.com>
2022-09-14 17:52:34 +0430

    adds build option to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,12 @@ docker-compose -f docker-compose-stage.yaml down --remove-orphans
 
 docker rmi botinow-backend-app:latest
 
+if  [ "$1" = "--build" ] ; then
+    echo "Building the images"
+    docker-compose -f docker-compose-stage.yaml build
+else
+    echo "Pulls the tagged images without building from local registry."
+fi
 docker-compose -f docker-compose-stage.yaml pull
 
 COMPOSE_HTTP_TIMEOUT=300 docker-compose -f docker-compose-stage.yaml up -d --no-build
