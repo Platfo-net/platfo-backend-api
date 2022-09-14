756cdcc549977596072561008af3ac48753e44cc
Mohammad Serpoush <m.serpoush@sfg.ir>
2022-09-14 19:52:02 +0430

    save message

--- app/core/tasks.py
+++ app/core/tasks.py
@@ -67,7 +67,8 @@ def save_message(obj_in: dict, instagram_page_id: str = None):
             last_message=obj_in["content"]
         )
 
-    services.message.create(db, obj_in=schemas.MessageCreate(
+    
+    report = services.message.create(db, obj_in=schemas.MessageCreate(
         from_page_id=obj_in["from_page_id"],
         to_page_id=obj_in["to_page_id"],
         content=obj_in["content"],
@@ -75,6 +76,8 @@ def save_message(obj_in: dict, instagram_page_id: str = None):
         user_id=obj_in["user_id"],
         direction=obj_in["direction"]
     ))
+    print(report)
+
 
 
 @celery.task
