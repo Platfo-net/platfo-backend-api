d06bd631653f9c2f44ad66761ecd653ab318f842
Mohammad Serpoush <m.serpoush@sfg.ir>
2022-09-15 22:05:24 +0430

    add one another instance

--- startup.sh
+++ startup.sh
@@ -6,4 +6,4 @@ alembic upgrade head
 python ./app/initial_data.py
 #Start app
 
-gunicorn app.main:app --workers 3 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+gunicorn app.main:app --workers 6 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
