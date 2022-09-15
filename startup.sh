de4c54d3d261834f2d81e78aca48e36ad68c3e20
Mohammad Serpoush <m.serpoush@sfg.ir>
2022-09-15 21:25:33 +0430

    change to gunicorn

--- startup.sh
+++ startup.sh
@@ -6,4 +6,4 @@ alembic upgrade head
 python ./app/initial_data.py
 #Start app
 
-gunicorn app.main:app --workers 6 -b 0.0.0.0:8000
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
