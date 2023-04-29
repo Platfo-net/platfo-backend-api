9f0ce3d0c6e7d9823091d41609948608ff6e22c6
mohammad serpoush <mohammad.serpush@gmail.com>
2023-04-29 12:47:20 +0330

    fix bug

--- app/schemas/token.py
+++ app/schemas/token.py
@@ -1,6 +1,24 @@
 from pydantic import BaseModel, validator
 from app.schemas.role import Role
 
+def normalize_phone_country_code(phone_country_code):
+    if not phone_country_code:
+        return phone_country_code
+
+    new_phone_country_code = phone_country_code
+    if phone_country_code[0:2] == "00":
+        new_phone_country_code = phone_country_code[2:]
+    if phone_country_code[0] == "+":
+        new_phone_country_code = phone_country_code[1:]
+    return new_phone_country_code
+
+def normalize_phone_number(phone_number):
+    if not phone_number:
+        return phone_number
+    if phone_number[0] == "0":
+        phone_number = phone_number[1:]
+    return phone_number
+
 
 class Token(BaseModel):
     access_token: str
@@ -33,10 +51,8 @@ class LoginFormByPhoneNumber(BaseModel):
 
     @validator("phone_number", always=True)
     def validate_phone_number(cls, phone_number, values):
-        pass
-        # return utils.normalize_phone_number(phone_number)
+        return normalize_phone_number(phone_number)
 
     @validator("phone_country_code", always=True)
     def validate_phone_country_code(cls, phone_country_code, values):
-        pass
-        # return utils.normalize_phone_country_code(phone_country_code)
+        return normalize_phone_country_code(phone_country_code)
