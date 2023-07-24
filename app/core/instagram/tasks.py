9b1981a54a0178565db8bc9436e2970bdbbc06cf
mohammad serpoush <mohammad.serpush@gmail.com>
2023-07-25 02:07:40 +0330

    remove extra codes

--- app/core/instagram/tasks.py
+++ app/core/instagram/tasks.py
@@ -35,14 +35,6 @@ def webhook_proccessor(facebook_webhook_body):
 
     handler = None
 
-    from app.core.instagram.graph_api import graph_api
-    if instagram_data.type == WebhookType.CONTACT_MESSAGE:
-        a = graph_api.send_persistant_menu(
-            from_id=user_page_data.facebook_page_id,
-            to_id=instagram_data.sender_id,
-            page_access_token=user_page_data.facebook_page_token)
-        print(a)
-        return
     match instagram_data.type:
         case WebhookType.MESSAGE_SEEN:
             handler = MessageSeenHandler(instagram_data, user_page_data, redis_client, db)
