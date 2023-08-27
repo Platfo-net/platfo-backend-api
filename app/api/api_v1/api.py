b4a104094150602c5bf1858c214df08891d4afa9
mohammad serpoush <mohammad.serpush@gmail.com>
2023-08-27 20:58:34 +0330

    add telegram bot submission api

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -3,8 +3,8 @@ from fastapi import APIRouter
 from app.api.api_v1.routers import (academy, accounts, auth, bot_builder_api,
                                     connection, constants, credit_api,
                                     databoard, dev_utils, file, instagram,
-                                    live_chat_api, notification, notifier_api,
-                                    users, webhook , telegram)
+                                    live_chat_api, notification, notifier_api, telegram_bot,
+                                    users, webhook)
 
 api_router = APIRouter()
 
@@ -21,7 +21,7 @@ api_router.include_router(live_chat_api.router)
 api_router.include_router(notifier_api.router)
 api_router.include_router(academy.router)
 api_router.include_router(databoard.router)
-api_router.include_router(telegram.router)
+api_router.include_router(telegram_bot.router)
 api_router.include_router(dev_utils.router)
 
 api_router.include_router(file.router)
