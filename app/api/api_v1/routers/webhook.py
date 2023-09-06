ce8747c719571be531eceb7c84c2cd82dd0d7afd
mohammad serpoush <mohammad.serpush@gmail.com>
2023-09-06 14:01:34 +0330

    complete support connect process

--- app/api/api_v1/routers/webhook.py
+++ app/api/api_v1/routers/webhook.py
@@ -67,13 +67,19 @@ async def telegram_webhook_support_listener(
     if update.message.text == "/start":
         await update.message.reply_text("Enter your code")
     else:
-        code = update.message.text
+        code = update.message.text.lstrip().rstrip()
+        if len(code) != 8:
+            await update.message.reply_text(f"Wrong code.")
+            return
         shop_telegram_bot = services.shop.shop_telegram_bot.get_by_support_token(
-            db, support_token=code.replace(" ", ""))
+            db, support_token=code)
         if not shop_telegram_bot:
             await update.message.reply_text(f"Wrong code.")
             return
-
+        if shop_telegram_bot.is_support_verified:
+            await update.message.reply_text(f"Your shop is already connected to an account.")
+            return
+            
         await update.message.reply_text(f"Enter this code in app: {shop_telegram_bot.support_bot_token}")
         services.shop.shop_telegram_bot.set_support_account_chat_id(
             db, db_obj=shop_telegram_bot, chat_id=update.message.chat_id)
