4cc00205a963cd2aa63dac75088ba75a081e125f
mohammad serpoush <mohammad.serpush@gmail.com>
2023-09-06 13:21:13 +0330

    add shop telegram router

--- app/api/api_v1/routers/shop_api.py
+++ app/api/api_v1/routers/shop_api.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.routers.shop import category, product, shop
+from app.api.api_v1.routers.shop import category, product, shop, shop_telegram
 
 router = APIRouter(prefix='/shop', tags=['Shop'])
 
 router.include_router(shop.router)
 router.include_router(product.router)
 router.include_router(category.router)
+
+router.include_router(shop_telegram.router)
