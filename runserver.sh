bce3c89226634aec7f2842739f2d1cc06bf42552
mohammad serpoush <mohammad.serpush@gmail.com>
2023-10-30 15:26:58 +0330

    add some prod settings

--- runserver.sh
+++ runserver.sh
@@ -7,4 +7,4 @@ alembic upgrade head
 
 python ./app/initial_data.py
 
-gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$1
+gunicorn app.main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$1
