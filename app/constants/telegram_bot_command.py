ea43530025a1b7925574cabfdd06c4078bfa01ee
mohammad serpoush <mohammad.serpush@gmail.com>
2023-11-05 15:04:23 +0330

    add vitrin

--- app/constants/telegram_bot_command.py
+++ app/constants/telegram_bot_command.py
@@ -8,10 +8,10 @@ class TelegramBotCommand:
         "command": "/send_direct_message_helper",
         "description": "ارسال پیام مستقیم به پشتیبانی"
     }
-    
+
     VITRIN = {
         "command": "/vitrin",
         "description": "ویترین"
     }
 
-    commands = [START, SEND_DIRECT_MESSAGE]
+    commands = [START, SEND_DIRECT_MESSAGE, VITRIN]
