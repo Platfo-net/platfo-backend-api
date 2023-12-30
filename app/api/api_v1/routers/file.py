a086f4b4c2f6d512692913ccd98e57190fe680fe
Mohammad Serpoush <mohamad.serpush@karnameh.ir>
2023-12-30 16:40:27 +0330

    Add category image upload endpoint

--- app/api/api_v1/routers/file.py
+++ app/api/api_v1/routers/file.py
@@ -8,22 +8,22 @@ from app.constants.role import Role
 from app.core import storage
 from app.core.config import settings
 
-router = APIRouter(prefix='/file', tags=['File'])
+router = APIRouter(prefix="/file", tags=["File"])
 
 
-@router.post('/upload/notifier/campaign', response_model=schemas.Image)
+@router.post("/upload/notifier/campaign", response_model=schemas.Image)
 async def upload_notifier_campaign_image(
     file: UploadFile = File(...),
     _: models.User = Security(
         deps.get_current_active_user,
         scopes=[
-            Role.ADMIN['name'],
-            Role.USER['name'],
-            Role.DEVELOPER['name'],
+            Role.ADMIN["name"],
+            Role.USER["name"],
+            Role.DEVELOPER["name"],
         ],
     ),
 ):
-    filename = f'{uuid.uuid4()}-{file.filename}'
+    filename = f"{uuid.uuid4()}-{file.filename}"
     uploaded_file_name = storage.add_file_to_s3(
         filename, file.file.fileno(), settings.S3_CAMPAIGN_BUCKET
     )
@@ -31,19 +31,19 @@ async def upload_notifier_campaign_image(
     return storage.get_file(uploaded_file_name, settings.S3_CAMPAIGN_BUCKET)
 
 
-@router.post('/upload/user/profile', response_model=schemas.Image)
+@router.post("/upload/user/profile", response_model=schemas.Image)
 async def upload_user_profile_image(
     file: UploadFile = File(...),
     _: models.User = Security(
         deps.get_current_active_user,
         scopes=[
-            Role.ADMIN['name'],
-            Role.USER['name'],
-            Role.DEVELOPER['name'],
+            Role.ADMIN["name"],
+            Role.USER["name"],
+            Role.DEVELOPER["name"],
         ],
     ),
 ):
-    filename = f'{uuid.uuid4()}-{file.filename}'
+    filename = f"{uuid.uuid4()}-{file.filename}"
     uploaded_file_name = storage.add_file_to_s3(
         filename, file.file.fileno(), settings.S3_USER_PROFILE_BUCKET
     )
@@ -51,19 +51,19 @@ async def upload_user_profile_image(
     return storage.get_file(uploaded_file_name, settings.S3_CAMPAIGN_BUCKET)
 
 
-@router.post('/upload/shop/product', response_model=schemas.Image)
+@router.post("/upload/shop/product", response_model=schemas.Image)
 async def upload_shop_product_image(
     file: UploadFile = File(...),
     _: models.User = Security(
         deps.get_current_active_user,
         scopes=[
-            Role.ADMIN['name'],
-            Role.USER['name'],
-            Role.DEVELOPER['name'],
+            Role.ADMIN["name"],
+            Role.USER["name"],
+            Role.DEVELOPER["name"],
         ],
     ),
 ):
-    filename = f'{uuid.uuid4()}-{file.filename}'
+    filename = f"{uuid.uuid4()}-{file.filename}"
     uploaded_file_name = storage.add_file_to_s3(
         filename, file.file.fileno(), settings.S3_SHOP_PRODUCT_IMAGE_BUCKET
     )
@@ -71,19 +71,39 @@ async def upload_shop_product_image(
     return storage.get_file(uploaded_file_name, settings.S3_SHOP_PRODUCT_IMAGE_BUCKET)
 
 
-@router.post('/upload/shop/order/payment-receipt', response_model=schemas.Image)
+@router.post("/upload/shop/product", response_model=schemas.Image)
+async def upload_shop_category_image(
+    file: UploadFile = File(...),
+    _: models.User = Security(
+        deps.get_current_active_user,
+        scopes=[
+            Role.ADMIN["name"],
+            Role.USER["name"],
+            Role.DEVELOPER["name"],
+        ],
+    ),
+):
+    filename = f"{uuid.uuid4()}-{file.filename}"
+    uploaded_file_name = storage.add_file_to_s3(
+        filename, file.file.fileno(), settings.S3_SHOP_CATEGORY_IMAGE_BUCKET
+    )
+
+    return storage.get_file(uploaded_file_name, settings.S3_SHOP_CATEGORY_IMAGE_BUCKET)
+
+
+@router.post("/upload/shop/order/payment-receipt", response_model=schemas.Image)
 async def upload_payment_receipt_image(
     file: UploadFile = File(...),
     _: models.User = Security(
         deps.get_current_active_user,
         scopes=[
-            Role.ADMIN['name'],
-            Role.USER['name'],
-            Role.DEVELOPER['name'],
+            Role.ADMIN["name"],
+            Role.USER["name"],
+            Role.DEVELOPER["name"],
         ],
     ),
 ):
-    filename = f'{uuid.uuid4()}-{file.filename}'
+    filename = f"{uuid.uuid4()}-{file.filename}"
     uploaded_file_name = storage.add_file_to_s3(
         filename, file.file.fileno(), settings.S3_PAYMENT_RECEIPT_IMAGE
     )
--- app/api/api_v1/routers/shop/shipment_method.py
+++ app/api/api_v1/routers/shop/shipment_method.py
@@ -9,24 +9,23 @@ from app.api import deps
 from app.constants.errors import Error
 from app.constants.role import Role
 from app.core.exception import raise_http_exception
-from app.core.unit_of_work import UnitOfWork
 
-router = APIRouter(prefix='/shipment-methods')
+router = APIRouter(prefix="/shipment-methods")
 
 
-@router.post('', response_model=schemas.shop.ShipmentMethod)
+@router.post("", response_model=schemas.shop.ShipmentMethod)
 def create_shipment_method(
-        *,
-        db: Session = Depends(deps.get_db),
-        obj_in: schemas.shop.ShipmentMethodCreate,
-        current_user: models.User = Security(
-            deps.get_current_active_user,
-            scopes=[
-                Role.USER['name'],
-                Role.ADMIN['name'],
-                Role.DEVELOPER['name'],
-            ],
-        ),
+    *,
+    db: Session = Depends(deps.get_db),
+    obj_in: schemas.shop.ShipmentMethodCreate,
+    current_user: models.User = Security(
+        deps.get_current_active_user,
+        scopes=[
+            Role.USER["name"],
+            Role.ADMIN["name"],
+            Role.DEVELOPER["name"],
+        ],
+    ),
 ):
     shop = services.shop.shop.get_by_uuid(db, uuid=obj_in.shop_id)
     if not shop:
@@ -36,9 +35,7 @@ def create_shipment_method(
         raise_http_exception(Error.SHOP_SHOP_NOT_FOUND_ACCESS_DENIED_ERROR)
 
     shop_shipment_method = services.shop.shipment_method.create(
-        db,
-        obj_in=obj_in,
-        shop_id=shop.id
+        db, obj_in=obj_in, shop_id=shop.id
     )
 
     return schemas.shop.ShipmentMethod(
@@ -50,19 +47,19 @@ def create_shipment_method(
     )
 
 
-@router.get('/{shop_id}/all', response_model=List[schemas.shop.ShipmentMethod])
+@router.get("/{shop_id}/all", response_model=List[schemas.shop.ShipmentMethod])
 def get_shop_shipment_methods(
-        *,
-        db: Session = Depends(deps.get_db),
-        shop_id: UUID4,
-        current_user: models.User = Security(
-            deps.get_current_active_user,
-            scopes=[
-                Role.USER['name'],
-                Role.ADMIN['name'],
-                Role.DEVELOPER['name'],
-            ],
-        ),
+    *,
+    db: Session = Depends(deps.get_db),
+    shop_id: UUID4,
+    current_user: models.User = Security(
+        deps.get_current_active_user,
+        scopes=[
+            Role.USER["name"],
+            Role.ADMIN["name"],
+            Role.DEVELOPER["name"],
+        ],
+    ),
 ):
     shop = services.shop.shop.get_by_uuid(db, uuid=shop_id)
     if not shop:
@@ -71,7 +68,9 @@ def get_shop_shipment_methods(
     if shop.user_id != current_user.id:
         raise_http_exception(Error.SHOP_SHOP_NOT_FOUND_ACCESS_DENIED_ERROR)
 
-    shop_shipment_methods = services.shop.shipment_method.get_multi_by_shop_id(db, shop_id=shop.id)
+    shop_shipment_methods = services.shop.shipment_method.get_multi_by_shop_id(
+        db, shop_id=shop.id
+    )
 
     return [
         schemas.shop.ShipmentMethod(
@@ -85,19 +84,19 @@ def get_shop_shipment_methods(
     ]
 
 
-@router.get('/{id}', response_model=schemas.shop.ShipmentMethod)
+@router.get("/{id}", response_model=schemas.shop.ShipmentMethod)
 def get_shipment_method(
-        *,
-        db: Session = Depends(deps.get_db),
-        id: UUID4,
-        current_user: models.User = Security(
-            deps.get_current_active_user,
-            scopes=[
-                Role.USER['name'],
-                Role.ADMIN['name'],
-                Role.DEVELOPER['name'],
-            ],
-        ),
+    *,
+    db: Session = Depends(deps.get_db),
+    id: UUID4,
+    current_user: models.User = Security(
+        deps.get_current_active_user,
+        scopes=[
+            Role.USER["name"],
+            Role.ADMIN["name"],
+            Role.DEVELOPER["name"],
+        ],
+    ),
 ):
     shop_shipment_method = services.shop.shipment_method.get_by_uuid(db, uuid=id)
     if not shop_shipment_method:
@@ -115,20 +114,20 @@ def get_shipment_method(
     )
 
 
-@router.put('/{id}', response_model=schemas.shop.ShipmentMethod)
+@router.put("/{id}", response_model=schemas.shop.ShipmentMethod)
 def update_shipment_method(
-        *,
-        db: Session = Depends(deps.get_db),
-        obj_in: schemas.shop.ShipmentMethodUpdate,
-        id: UUID4,
-        current_user: models.User = Security(
-            deps.get_current_active_user,
-            scopes=[
-                Role.USER['name'],
-                Role.ADMIN['name'],
-                Role.DEVELOPER['name'],
-            ],
-        ),
+    *,
+    db: Session = Depends(deps.get_db),
+    obj_in: schemas.shop.ShipmentMethodUpdate,
+    id: UUID4,
+    current_user: models.User = Security(
+        deps.get_current_active_user,
+        scopes=[
+            Role.USER["name"],
+            Role.ADMIN["name"],
+            Role.DEVELOPER["name"],
+        ],
+    ),
 ):
     shop_shipment_method = services.shop.shipment_method.get_by_uuid(db, uuid=id)
     if not shop_shipment_method:
@@ -138,7 +137,8 @@ def update_shipment_method(
         raise_http_exception(Error.SHOP_SHIPMENT_METHOD_NOT_FOUND_ERROR_ACCESS_DENIED)
 
     shop_shipment_method = services.shop.shipment_method.update(
-        db, db_obj=shop_shipment_method, obj_in=obj_in)
+        db, db_obj=shop_shipment_method, obj_in=obj_in
+    )
 
     return schemas.shop.ShipmentMethod(
         id=shop_shipment_method.uuid,
@@ -149,20 +149,20 @@ def update_shipment_method(
     )
 
 
-@router.put('/{id}/change-is-active', status_code=status.HTTP_200_OK)
+@router.put("/{id}/change-is-active", status_code=status.HTTP_200_OK)
 def change_shop_shipment_method_is_active(
-        *,
-        db: Session = Depends(deps.get_db),
-        id: UUID4,
-        obj_in: schemas.shop.ChangeShipmentIsActive,
-        current_user: models.User = Security(
-            deps.get_current_active_user,
-            scopes=[
-                Role.USER['name'],
-                Role.ADMIN['name'],
-                Role.DEVELOPER['name'],
-            ],
-        ),
+    *,
+    db: Session = Depends(deps.get_db),
+    id: UUID4,
+    obj_in: schemas.shop.ChangeShipmentIsActive,
+    current_user: models.User = Security(
+        deps.get_current_active_user,
+        scopes=[
+            Role.USER["name"],
+            Role.ADMIN["name"],
+            Role.DEVELOPER["name"],
+        ],
+    ),
 ):
     shop_shipment_method = services.shop.shipment_method.get_by_uuid(db, uuid=id)
     if not shop_shipment_method:
@@ -172,7 +172,8 @@ def change_shop_shipment_method_is_active(
         raise_http_exception(Error.SHOP_SHIPMENT_METHOD_NOT_FOUND_ERROR_ACCESS_DENIED)
 
     shop_shipment_method = services.shop.shipment_method.change_is_active(
-        db, obj_in=shop_shipment_method, is_active=obj_in.is_active)
+        db, obj_in=shop_shipment_method, is_active=obj_in.is_active
+    )
 
     schemas.shop.ShipmentMethod(
         id=shop_shipment_method.uuid,
@@ -183,19 +184,19 @@ def change_shop_shipment_method_is_active(
     )
 
 
-@router.delete('/{id}', status_code=status.HTTP_200_OK)
+@router.delete("/{id}", status_code=status.HTTP_200_OK)
 def delete_shipment_method(
-        *,
-        db: Session = Depends(deps.get_db),
-        id: UUID4,
-        current_user: models.User = Security(
-            deps.get_current_active_user,
-            scopes=[
-                Role.USER['name'],
-                Role.ADMIN['name'],
-                Role.DEVELOPER['name'],
-            ],
-        ),
+    *,
+    db: Session = Depends(deps.get_db),
+    id: UUID4,
+    current_user: models.User = Security(
+        deps.get_current_active_user,
+        scopes=[
+            Role.USER["name"],
+            Role.ADMIN["name"],
+            Role.DEVELOPER["name"],
+        ],
+    ),
 ):
     shop_shipment_method = services.shop.shipment_method.get_by_uuid(db, uuid=id)
     if not shop_shipment_method:
@@ -209,18 +210,20 @@ def delete_shipment_method(
     return
 
 
-@router.get('/telegram/{shop_id}/all', response_model=List[schemas.shop.ShipmentMethod])
+@router.get("/telegram/{shop_id}/all", response_model=List[schemas.shop.ShipmentMethod])
 def get_telegram_shop_shipment_methods(
-        *,
-        db: Session = Depends(deps.get_db),
-        shop_id: UUID4,
+    *,
+    db: Session = Depends(deps.get_db),
+    shop_id: UUID4,
 ):
     shop = services.shop.shop.get_by_uuid(db, uuid=shop_id)
 
     if not shop:
         raise_http_exception(Error.SHOP_SHOP_NOT_FOUND_ERROR)
 
-    shop_shipment_methods = services.shop.shipment_method.get_multi_by_shop_id(db, shop_id=shop.id)
+    shop_shipment_methods = services.shop.shipment_method.get_multi_by_shop_id(
+        db, shop_id=shop.id
+    )
 
     return [
         schemas.shop.ShipmentMethod(
@@ -234,19 +237,23 @@ def get_telegram_shop_shipment_methods(
     ]
 
 
-@router.get('/telegram/{shop_id}/{shipment_id}', response_model=schemas.shop.ShipmentMethod)
+@router.get(
+    "/telegram/{shop_id}/{shipment_id}", response_model=schemas.shop.ShipmentMethod
+)
 def get_telegram_shop_shipment_method(
-        *,
-        db: Session = Depends(deps.get_db),
-        shop_id: UUID4,
-        shipment_id: UUID4,
+    *,
+    db: Session = Depends(deps.get_db),
+    shop_id: UUID4,
+    shipment_id: UUID4,
 ):
     shop = services.shop.shop.get_by_uuid(db, uuid=shop_id)
 
     if not shop:
         raise_http_exception(Error.SHOP_SHOP_NOT_FOUND_ERROR)
 
-    shop_shipment_method = services.shop.shipment_method.get_by_uuid(db, uuid=shipment_id)
+    shop_shipment_method = services.shop.shipment_method.get_by_uuid(
+        db, uuid=shipment_id
+    )
 
     if not shop_shipment_method:
         raise_http_exception(Error.SHOP_SHIPMENT_METHOD_NOT_FOUND_ERROR)
