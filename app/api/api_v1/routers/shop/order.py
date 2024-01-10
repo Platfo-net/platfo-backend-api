79925f2910ffa930565a8bb563073054860d7d3b
mohammad.serpush@gmail.com <51956677+mohammad-Serpoush@users.noreply.github.com>
2024-01-10 11:42:59 +0330

    Add status to order (#63)
    Add status to order

--- app/api/api_v1/routers/shop/order.py
+++ app/api/api_v1/routers/shop/order.py
@@ -15,10 +15,12 @@ from app.core.exception import raise_http_exception
 from app.core.telegram import tasks as telegram_tasks
 from app.core.unit_of_work import UnitOfWork
 
-router = APIRouter(prefix='/orders')
+router = APIRouter(prefix="/orders")
 
 
-@router.post("/telegram/{shop_id}/{lead_id}", response_model=schemas.shop.OrderCreateResponse)
+@router.post(
+    "/telegram/{shop_id}/{lead_id}", response_model=schemas.shop.OrderCreateResponse
+)
 def create_telegram_shop_order(
     *,
     db: Session = Depends(deps.get_db),
@@ -36,32 +38,34 @@ def create_telegram_shop_order(
     #     raise_http_exception(Error.LEAD_TELEGRAM_LEAD_NOT_FOUND)
 
     shop_telegram_bot = services.shop.shop_telegram_bot.get_by_shop_id(
-        db, shop_id=shop.id)
+        db, shop_id=shop.id
+    )
     if lead.telegram_bot_id != shop_telegram_bot.telegram_bot_id:
         raise_http_exception(Error.LEAD_TELEGRAM_LEAD_NOT_FOUND_ACCESS_DENIED)
 
     shop_payment_method = services.shop.shop_payment_method.get_by_uuid(
-        db, uuid=obj_in.payment_method_id)
+        db, uuid=obj_in.payment_method_id
+    )
     if not shop_payment_method:
         raise_http_exception(Error.SHOP_PAYMENT_METHOD_NOT_FOUND_ERROR)
 
     if shop_payment_method.shop_id != shop.id:
-        raise_http_exception(
-            Error.SHOP_PAYMENT_METHOD_NOT_FOUND_ERROR_ACCESS_DENIED)
+        raise_http_exception(Error.SHOP_PAYMENT_METHOD_NOT_FOUND_ERROR_ACCESS_DENIED)
 
     shipment_method = services.shop.shipment_method.get_by_uuid(
-        db, uuid=obj_in.shipment_method_id)
+        db, uuid=obj_in.shipment_method_id
+    )
     if not shipment_method:
         raise_http_exception(Error.SHOP_SHIPMENT_METHOD_NOT_FOUND_ERROR)
 
     if shipment_method.shop_id != shop.id:
-        raise_http_exception(
-            Error.SHOP_SHIPMENT_METHOD_NOT_FOUND_ERROR_ACCESS_DENIED)
+        raise_http_exception(Error.SHOP_SHIPMENT_METHOD_NOT_FOUND_ERROR_ACCESS_DENIED)
 
-    with UnitOfWork(db)as uow:
+    with UnitOfWork(db) as uow:
         order_items = []
         last_order_number = services.shop.order.get_last_order_number(
-            db, shop_id=shop.id)
+            db, shop_id=shop.id
+        )
 
         order = services.shop.order.create(
             uow,
@@ -74,34 +78,33 @@ def create_telegram_shop_order(
             status=OrderStatus.UNPAID["value"],
         )
 
-    with UnitOfWork(db)as uow:
+    with UnitOfWork(db) as uow:
         for item in obj_in.items:
-            product = services.shop.product.get_by_uuid(
-                db, uuid=item.product_id)
+            product = services.shop.product.get_by_uuid(db, uuid=item.product_id)
             if not product or product.shop_id != shop.id:
                 raise_http_exception(Error.SHOP_PRODUCT_NOT_FOUND_ERROR)
-            order_items.append(schemas.shop.OrderItem(
-                product_id=product.id,
-                count=item.count,
-                price=product.price,
-                currency=product.currency,
-            ))
+            order_items.append(
+                schemas.shop.OrderItem(
+                    product_id=product.id,
+                    count=item.count,
+                    price=product.price,
+                    currency=product.currency,
+                )
+            )
         services.shop.order_item.create_bulk(
-            uow, objs_in=order_items, order_id=order.id)
-        telegram_order = services.shop.telegram_order.create(
-            uow, order_id=order.id)
+            uow, objs_in=order_items, order_id=order.id
+        )
+        telegram_order = services.shop.telegram_order.create(uow, order_id=order.id)
 
     telegram_tasks.send_lead_order_to_bot_and_support_bot_task.delay(
         shop_telegram_bot.telegram_bot.id,
         lead.id if lead else None,
         order.id,
         telegram_order.id,
-        "fa"
+        "fa",
     )
 
-    return schemas.shop.order.OrderCreateResponse(
-        order_number=str(order.order_number)
-    )
+    return schemas.shop.order.OrderCreateResponse(order_number=str(order.order_number))
 
 
 @router.get("/{shop_id}/all", response_model=schemas.shop.OrderListApiResponse)
@@ -114,9 +117,9 @@ def get_orders_by_shop_id(
     current_user: models.User = Security(
         deps.get_current_active_user,
         scopes=[
-            Role.ADMIN['name'],
-            Role.USER['name'],
-            Role.DEVELOPER['name'],
+            Role.ADMIN["name"],
+            Role.USER["name"],
+            Role.DEVELOPER["name"],
         ],
     ),
 ):
@@ -128,7 +131,8 @@ def get_orders_by_shop_id(
         raise_http_exception(Error.SHOP_SHOP_NOT_FOUND_ACCESS_DENIED_ERROR)
 
     orders, pagination = services.shop.order.get_multi_by_shop_id(
-        db, shop_id=shop.id, page=page, page_size=page_size)
+        db, shop_id=shop.id, page=page, page_size=page_size
+    )
 
     orders_list = []
     for order in orders:
@@ -136,19 +140,27 @@ def get_orders_by_shop_id(
         for item in order.items:
             sum += item.price * item.count
 
-        orders_list.append(schemas.shop.OrderListItem(
-            id=order.uuid,
-            order_number=order.order_number,
-            first_name=order.first_name,
-            last_name=order.last_name,
-            phone_number=order.phone_number,
-            city=order.city,
-            total_amount=sum,
-            currency=Currency.IRT["name"],
-            created_at=order.created_at,
-            payment_method=PaymentMethod.items[order.shop_payment_method.payment_method.title]["fa"] if order.shop_payment_method else None,  # noqa
-            shipment_method=order.shipment_method.title if order.shipment_method else None,
-        )
+        orders_list.append(
+            schemas.shop.OrderListItem(
+                id=order.uuid,
+                order_number=order.order_number,
+                first_name=order.first_name,
+                last_name=order.last_name,
+                phone_number=order.phone_number,
+                city=order.city,
+                total_amount=sum,
+                currency=Currency.IRT["name"],
+                created_at=order.created_at,
+                payment_method=PaymentMethod.items[
+                    order.shop_payment_method.payment_method.title
+                ]["fa"]
+                if order.shop_payment_method
+                else None,  # noqa
+                shipment_method=order.shipment_method.title
+                if order.shipment_method
+                else None,
+                status=OrderStatus.items[order.status]["title"]["fa"],
+            )
         )
 
     return schemas.shop.OrderListApiResponse(
@@ -165,13 +177,12 @@ def get_order(
     current_user: models.User = Security(
         deps.get_current_active_user,
         scopes=[
-            Role.ADMIN['name'],
-            Role.USER['name'],
-            Role.DEVELOPER['name'],
+            Role.ADMIN["name"],
+            Role.USER["name"],
+            Role.DEVELOPER["name"],
         ],
     ),
 ):
-
     order = services.shop.order.get_by_uuid(db, uuid=order_id)
     if not order:
         raise_http_exception(Error.SHOP_ORDER_NOT_FOUND)
@@ -184,7 +195,8 @@ def get_order(
     for item in order.items:
         sum += item.price * item.count
         image_url = storage.get_object_url(
-            item.product.image, settings.S3_SHOP_PRODUCT_IMAGE_BUCKET)
+            item.product.image, settings.S3_SHOP_PRODUCT_IMAGE_BUCKET
+        )
         items.append(
             schemas.shop.OrderItemResponse(
                 count=item.count,
@@ -208,8 +220,14 @@ def get_order(
         state=order.state,
         city=order.city,
         items=items,
-        payment_method=PaymentMethod.items[order.shop_payment_method.payment_method.title]["fa"] if order.shop_payment_method else None,  # noqa
+        payment_method=PaymentMethod.items[
+            order.shop_payment_method.payment_method.title
+        ]["fa"]
+        if order.shop_payment_method
+        else None,  # noqa
         shipment_method=order.shipment_method.title,
+        status=OrderStatus.items[order.status]["title"]["fa"],
+        payment_information=order.payment_information,
     )
 
 
@@ -222,13 +240,12 @@ def change_order_status(
     current_user: models.User = Security(
         deps.get_current_active_user,
         scopes=[
-            Role.ADMIN['name'],
-            Role.USER['name'],
-            Role.DEVELOPER['name'],
+            Role.ADMIN["name"],
+            Role.USER["name"],
+            Role.DEVELOPER["name"],
         ],
     ),
 ):
-
     order = services.shop.order.get_by_uuid(db, uuid=order_id)
     if not order:
         raise_http_exception(Error.SHOP_ORDER_NOT_FOUND)
@@ -239,15 +256,15 @@ def change_order_status(
     if not OrderStatus.items.get(obj_in.status):
         raise_http_exception(Error.SHOP_INVALID_ORDER_STATUS)
 
-    order = services.shop.order.change_status(
-        db, order=order, status=obj_in.status)
+    order = services.shop.order.change_status(db, order=order, status=obj_in.status)
 
     sum = 0
     items = []
     for item in order.items:
         sum += item.price * item.count
         image_url = storage.get_object_url(
-            item.product.image, settings.S3_SHOP_PRODUCT_IMAGE_BUCKET)
+            item.product.image, settings.S3_SHOP_PRODUCT_IMAGE_BUCKET
+        )
         items.append(
             schemas.shop.OrderItemResponse(
                 count=item.count,
@@ -273,6 +290,12 @@ def change_order_status(
         state=order.state,
         city=order.city,
         items=items,
-        payment_method=PaymentMethod.items[order.shop_payment_method.payment_method.title]["fa"] if order.shop_payment_method else None,  # noqa
+        payment_method=PaymentMethod.items[
+            order.shop_payment_method.payment_method.title
+        ]["fa"]
+        if order.shop_payment_method
+        else None,  # noqa
         shipment_method=order.shipment_method.title if order.shipment_method else None,
+        status=OrderStatus.items[order.status]["title"]["fa"],
+        payment_information=order.payment_information,
     )
