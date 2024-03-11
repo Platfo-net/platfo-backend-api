bde0fdfd5bbc33944904aca3f4aaef06b3b8acd6
mohammad <mohammad.serpush@gmail.com>
2024-03-11 11:02:08 +0300

    Add analytics and fix some bugs

--- app/core/shop_daily_report/tasks.py
+++ app/core/shop_daily_report/tasks.py
@@ -10,17 +10,28 @@ from sqlalchemy.orm import Session
 
 def calculate_shops_daily_report_task():
     db = SessionLocal()
-    shops = db.query(models.shop.ShopShop).all()
     from_datetime = datetime.now().astimezone(pytz.timezone("Asia/Tehran")).replace(
         hour=0, minute=0, second=0, microsecond=0
     )
     to_datetime = from_datetime + timedelta(days=1)
-    for shop in shops:
-        pass
+    analytics = calculate_shop_daily_report(db, from_datetime, to_datetime)
+    objs = []
+    for analytic in analytics:
+        obj = models.shop.ShopDailyReport(
+            order_count=analytic[2],
+            order_amount=analytic[1] if analytic[1] else 0,
+            date=from_datetime.date(),
+            shop_id=analytic[0]
+        )
+        objs.append(obj)
+        db.add_all(objs)
+        db.commit()
+
+    return len(objs)
 
 
 def calculate_shop_daily_report(db: Session, from_datetime, to_datetime):
-    orders = db.query(
+    report = db.query(
         models.shop.ShopOrder.shop_id,
         func.sum(models.shop.ShopOrder.total_amount),
         func.count(models.shop.ShopOrder.id),
@@ -28,5 +39,3 @@ def calculate_shop_daily_report(db: Session, from_datetime, to_datetime):
         models.shop.ShopOrder.created_at >= from_datetime,
         models.shop.ShopOrder.created_at < to_datetime,
     ).group_by(models.shop.ShopOrder.shop_id).all()
-    for order in orders:
-        pass
