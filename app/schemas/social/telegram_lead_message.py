20ddf6e7adba8df44b9635f6d98fca01bdfc270e
Mohammad Serpoush <mohamad.serpush@karnameh.ir>
2024-03-11 11:33:32 +0330

    Fix lead create schema

--- app/schemas/social/telegram_lead_message.py
+++ app/schemas/social/telegram_lead_message.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel
 
 
 class TelegramLeadMessageCreate(BaseModel):
-    lead_id: int
+    lead_id: Optional[int] = None
     is_lead_to_bot: bool
-    message: str
-    message_id: int
-    mirror_message_id: int
+    message: Optional[str] = None
+    message_id: Optional[int] = None
+    mirror_message_id: Optional[int] = None
     reply_to_id: Optional[int] = None
