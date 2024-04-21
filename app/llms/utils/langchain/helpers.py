9b060f23f8b01dc720fd1d3d67d9aa3c02e932a2
arianagh <arian1999@protonmail.com>
2024-04-21 09:43:16 -0700

    added temperature to chatbots and changed the logic of the prompt

--- app/llms/utils/langchain/helpers.py
+++ app/llms/utils/langchain/helpers.py
@@ -2,11 +2,11 @@ import os
 import re
 
 import tiktoken
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders.pdf import PyPDFLoader
 from langchain_community.document_loaders.text import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
+from langchain_core.runnables import RunnableLambda, RunnableParallel, RunnablePassthrough
 from langchain_openai.chat_models import ChatOpenAI
 
 from app.llms.utils import config
@@ -36,16 +36,14 @@ def load_text_document(file):
 def get_document_loader_data(file_path, file):
     _, extension = os.path.splitext(file_path)
     extension = extension.lower()
-    loaders = {
-        DocumentFormat.PDF: load_pdf_document,
-        DocumentFormat.TXT: load_text_document,
-    }
+    loaders = {DocumentFormat.PDF: load_pdf_document, DocumentFormat.TXT: load_text_document, }
     loader_func = loaders.get(extension)
     return loader_func(file)
 
 
-def chunk_data(data, metadatas ,chunk_size=256, chunk_overlap=100):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+def chunk_data(data, metadatas, chunk_size=256, chunk_overlap=100):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,
+                                                   chunk_overlap=chunk_overlap)
     texts = [d.page_content for d in data]
     metadatas = metadatas * len(texts)
     chunks = text_splitter.create_documents(texts=texts, metadatas=metadatas)
@@ -60,12 +58,12 @@ def print_embedding_cost(texts):
 
 
 def clear_text(text):
-  # Remove newlines (\n)
-  text = text.replace("\n", " ")
-  # Remove unnecessary punctuation
-  text = re.sub("[^\w\s]", "", text)
+    # Remove newlines (\n)
+    text = text.replace("\n", " ")
+    # Remove unnecessary punctuation
+    text = re.sub("[^\w\s]", "", text)  # noqa
 
-  return text
+    return text
 
 
 def get_chat_prompt():
@@ -80,11 +78,14 @@ def get_chat_prompt():
 
 
 def create_setup_retriever(retriever, prompt_callable):
-    setup_and_retrieval = RunnableParallel(
-        {"context": retriever, "query": RunnablePassthrough(), "user_prompt": RunnableLambda(prompt_callable)}
-    )
+    setup_and_retrieval = RunnableParallel({
+        "context": retriever,
+        "query": RunnablePassthrough(),
+        "user_prompt": RunnableLambda(prompt_callable)
+    })
     return setup_and_retrieval
 
 
-def create_llm_model():
-    return ChatOpenAI(openai_api_key=config.OPEN_API_KEY, model=config.LLM_MODEL) # type: ignore
+def create_llm_model(temperature: float):
+    return ChatOpenAI(openai_api_key=config.OPEN_API_KEY, model=config.LLM_MODEL,  # type: ignore
+                      temperature=temperature)
