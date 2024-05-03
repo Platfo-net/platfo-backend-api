0d17d4c353ee05c3aa4e79e04c07f4665b2ce571
mohammad <mohammad.serpush@gmail.com>
2024-05-03 18:01:03 +0300

    new mig

--- alembic/versions/36fe8849a271_0027_chatbot_telegram_bot_cascade.py
+++ alembic/versions/36fe8849a271_0027_chatbot_telegram_bot_cascade.py
@@ -1,8 +1,8 @@
 """0027_chatbot_telegram_bot_cascade
 
-Revision ID: 2d2690165750
+Revision ID: 36fe8849a271
 Revises: e610dde7d7fe
-Create Date: 2024-05-03 14:59:07.929705
+Create Date: 2024-05-03 15:00:49.654895
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2d2690165750'
+revision = '36fe8849a271'
 down_revision = 'e610dde7d7fe'
 branch_labels = None
 depends_on = None
